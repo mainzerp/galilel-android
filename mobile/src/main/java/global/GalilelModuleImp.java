a9e8669bea855a6ecd555a8275ea200e8e34d7a2
Maik Broemme <mbroemme@libmpq.org>
2019-03-02 19:13:27 +0100

    Disable privacy feature till proper implementation

--- mobile/src/main/java/global/GalilelModuleImp.java
+++ mobile/src/main/java/global/GalilelModuleImp.java
@@ -398,26 +398,12 @@ public class GalilelModuleImp implements GalilelModule {
             }
             TransactionWrapper wrapper;
             if (!isStaking){
-                // Check if a zc_spend
-                boolean isZcSpend = false;
-                for (TransactionInput transactionInput : transaction.getInputs()) {
-                    if (transactionInput.isZcspend()){
-                        isZcSpend = true;
-                        break;
-                    }
-                }
-                TransactionWrapper.TransactionUse transactionUse;
-                if (!isZcSpend)
-                    transactionUse = isMine ? TransactionWrapper.TransactionUse.SENT_SINGLE: TransactionWrapper.TransactionUse.RECEIVE;
-                else {
-                    transactionUse = TransactionWrapper.TransactionUse.ZC_SPEND;
-                }
                 wrapper = new TransactionWrapper(
                         transaction,
                         inputsLabeled,
                         outputsLabeled,
                         isMine ? getValueSentFromMe(transaction,true):walletManager.getValueSentToMe(transaction),
-                        transactionUse
+                        isMine ? TransactionWrapper.TransactionUse.SENT_SINGLE: TransactionWrapper.TransactionUse.RECEIVE
                 );
             }else {
                 wrapper = new TransactionWrapper(
