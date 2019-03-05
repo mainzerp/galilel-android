da19a6e04ef079afbfd8e5eef7e189202cdcd8c5
Maik Broemme <mbroemme@libmpq.org>
2019-03-05 21:27:52 +0100

    Added prefix +/- to transaction overview, fixed coloring for Zerocoin

--- app/src/main/java/galilel/org/galilelwallet/ui/wallet_activity/TransactionsFragmentBase.java
+++ app/src/main/java/galilel/org/galilelwallet/ui/wallet_activity/TransactionsFragmentBase.java
@@ -101,19 +101,23 @@ public class TransactionsFragmentBase extends BaseRecyclerFragment<TransactionWr
 
                 holder.description.setText(data.getTransaction().getMemo());
 
-                if (data.isSent()){
+                if (data.isSent()) {
                     //holder.cv.setBackgroundColor(Color.RED);Color.BROWN
                     holder.imageView.setImageResource(R.mipmap.ic_transaction_send);
                     holder.amount.setTextColor(ContextCompat.getColor(context, R.color.red));
-                }else if (data.isZcSpend()) {
+                    holder.amount.setText("-" + holder.amount.getText());
+                } else if (data.isZcSpend()) {
                     holder.imageView.setImageResource(R.drawable.ic_transaction_incognito);
-                    holder.amount.setTextColor(ContextCompat.getColor(context, R.color.brown));
-                } else if (!data.isStake()){
+                    holder.amount.setTextColor(ContextCompat.getColor(context, R.color.red));
+                    holder.amount.setText("-" + holder.amount.getText());
+                } else if (!data.isStake()) {
                     holder.imageView.setImageResource(R.mipmap.ic_transaction_receive);
                     holder.amount.setTextColor(ContextCompat.getColor(context, R.color.brown));
+                    holder.amount.setText("+" + holder.amount.getText());
                 } else {
                     holder.imageView.setImageResource(R.drawable.ic_transaction_mining);
                     holder.amount.setTextColor(ContextCompat.getColor(context, R.color.brown));
+                    holder.amount.setText("+" + holder.amount.getText());
                 }
                 holder.title.setText(getAddressOrContact(galilelModule,data));
 
