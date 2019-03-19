mainzerp/galilel-android

commit 03c05d7f325dddc233e106b7ca199fa8925cf60e

Layout and coloring adjustments:
- Renamed bgWhite color to galilelWhite
- Renamed 'bg_button_border' to 'button_brown'
- Renamed 'button_selector' to 'button_white'
- Renamed 'app/src/main/res/layout/sincyng_view.xml' to 'app/src/main/res/layout/syncing_view.xml'
- Added darkBrown2 color for onClick() of darkBrown1 buttons
- Added grey10black color for onClick() of galilelWhite buttons
- Added 'Add node' button on tutorial screen 4
- Removed 'ButtomWhite' from style
- Removed crappy looking ZillaSlab font family
- Removed unused CustomFontTextView class
- Replaced color codes with variables from 'app/src/main/res/values/colors.xml'
- Replaced color codes in multiple variables with single one
- Replaced all checkboxes with image versions to beautify checked and unchecked state
- Unify all layout screens with background color
- Unify all input fields with background and text color
- Unify all dropdown list icons

app/src/main/java/galilel/org/galilelwallet/ui/address_add_activity/AddContactActivity.java
@@ -60,13 +60,7 @@ public class AddContactActivity extends BaseActivity implements View.OnClickList
 
             @Override
             public void afterTextChanged(Editable s) {
-                String temp = s.toString();
-                if(galilelModule.chechAddress(temp)){
-                    address = temp;
-                    edit_address.setTextColor(Color.parseColor("#795548"));;
-                }else {
-                    edit_address.setTextColor(Color.parseColor("#4d4d4d"));;
-                }
+                edit_address.setTextColor(getResources().getColor(R.color.grey85black));
             }
         });
         Intent intent = getIntent();

commit 0305463768b7d8ff7b84563317f7f6be56b2c592

Fixed "Invalid address" bug introduced in 03c05d7f325dddc233e106b7ca199fa8925cf60

app/src/main/java/galilel/org/galilelwallet/ui/address_add_activity/AddContactActivity.java
@@ -61,6 +61,9 @@ public class AddContactActivity extends BaseActivity implements View.OnClickList
 
             @Override
             public void afterTextChanged(Editable s) {
+                String temp = s.toString();
+                if (galilelModule.chechAddress(temp))
+                    address = temp;
                 edit_address.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.grey85black));
             }
         });