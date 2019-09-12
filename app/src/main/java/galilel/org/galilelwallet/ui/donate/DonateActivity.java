e07853a3a12cc752208a2dee6897ba529943769e
Maik Broemme <mbroemme@libmpq.org>
2019-09-12 14:06:50 +0200

    Do all transaction checks before entering the PIN while donating

--- app/src/main/java/galilel/org/galilelwallet/ui/donate/DonateActivity.java
+++ app/src/main/java/galilel/org/galilelwallet/ui/donate/DonateActivity.java
@@ -33,6 +33,11 @@ public class DonateActivity extends BaseDrawerActivity {
     private Button btn_donate;
     private SimpleTextDialog errorDialog;
 
+    private String addressStr;
+    private String amountStr;
+    private Coin amount;
+    private Transaction transaction;
+
     @Override
     protected void onCreateView(Bundle savedInstanceState, ViewGroup container) {
         root = getLayoutInflater().inflate(R.layout.donations_fragment,container);
@@ -41,10 +46,19 @@ public class DonateActivity extends BaseDrawerActivity {
         btn_donate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // start pin
-                Intent intent = new Intent(DonateActivity.this, PincodeActivity.class);
-                intent.putExtra(PincodeActivity.CHECK_PIN,true);
-                startActivityForResult(intent,PIN_RESULT);
+                try {
+
+                    // checks
+                    send_prepare();
+
+                    // start pin
+                    Intent intent = new Intent(DonateActivity.this, PincodeActivity.class);
+                    intent.putExtra(PincodeActivity.CHECK_PIN,true);
+                    startActivityForResult(intent,PIN_RESULT);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    showErrorDialog(e.getMessage());
+                }
             }
         });
     }
@@ -53,50 +67,48 @@ public class DonateActivity extends BaseDrawerActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PIN_RESULT){
             if (resultCode == RESULT_OK){
-                try{
-                    send();
-                }catch (Exception e){
-                    e.printStackTrace();
-                    showErrorDialog(e.getMessage());
-                }
+                send();
             }
         }
     }
 
-    private void send() {
+    private void send_prepare() {
         try {
+
             // create the tx
-            String addressStr = GalilelContext.DONATE_ADDRESS;
+            addressStr = GalilelContext.DONATE_ADDRESS;
             if (!galilelModule.chechAddress(addressStr))
                 throw new IllegalArgumentException(getString(R.string.invalid_input_address));
-            String amountStr = edit_amount.getText().toString();
+            amountStr = edit_amount.getText().toString();
             if (amountStr.length() < 1) throw new IllegalArgumentException(getString(R.string.invalid_amount));
             if (amountStr.length()==1 && amountStr.equals(".")) throw new IllegalArgumentException(getString(R.string.invalid_amount));
             if (amountStr.charAt(0)=='.'){
                 amountStr = getString(R.string.number_0) + amountStr;
             }
-            Coin amount = Coin.parseCoin(amountStr);
+            amount = Coin.parseCoin(amountStr);
             if (amount.isZero()) throw new IllegalArgumentException(getString(R.string.invalid_amount));
             if (amount.isLessThan(Transaction.MIN_NONDUST_OUTPUT)) throw new IllegalArgumentException(getString(R.string.invalid_amount_small) + " " + Transaction.MIN_NONDUST_OUTPUT.toFriendlyString());
             if (amount.isGreaterThan(Coin.valueOf(galilelModule.getAvailableBalance())))
                 throw new IllegalArgumentException(getString(R.string.invalid_balance));
             String memo = "Donation!";
             // build a tx with the default fee
-            Transaction transaction = galilelModule.buildSendTx(addressStr, amount, memo,galilelModule.getReceiveAddress());
-            // send it
-            galilelModule.commitTx(transaction);
-            Intent intent = new Intent(DonateActivity.this, GalilelWalletService.class);
-            intent.setAction(ACTION_BROADCAST_TRANSACTION);
-            intent.putExtra(DATA_TRANSACTION_HASH,transaction.getHash().getBytes());
-            startService(intent);
-
-            Toast.makeText(this,getString(R.string.donation_thanks),Toast.LENGTH_LONG).show();
-            onBackPressed();
-
+            transaction = galilelModule.buildSendTx(addressStr, amount, memo,galilelModule.getReceiveAddress());
         } catch (InsufficientMoneyException e) {
             e.printStackTrace();
             throw new IllegalArgumentException(getString(R.string.invalid_balance));
         }
+    }
+
+    private void send() {
+        // send it
+        galilelModule.commitTx(transaction);
+        Intent intent = new Intent(DonateActivity.this, GalilelWalletService.class);
+        intent.setAction(ACTION_BROADCAST_TRANSACTION);
+        intent.putExtra(DATA_TRANSACTION_HASH,transaction.getHash().getBytes());
+        startService(intent);
+
+        Toast.makeText(this,getString(R.string.donation_thanks),Toast.LENGTH_LONG).show();
+        onBackPressed();
     }
 
     private void showErrorDialog(String message) {
